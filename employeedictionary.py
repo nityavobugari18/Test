e551c0307f3a79bd13954183faf342de421dbb3f
nityavobugari18 <nityavobugari@gmail.com>
2026-01-21 09:59:49 +0530

    Employee dictionary

---
+++ employeedictionary.py
@@ -0,0 +1,14 @@
+employee_list = []
+
+n = int(input("Enter number of employees: "))
+
+for i in range(n):
+    name = input("Enter name: ")
+    salary = int(input("Enter salary: "))
+    employee_list.append((name, salary))
+
+employee_dictionary = dict(employee_list)
+
+print(employee_dictionary)
+
+
